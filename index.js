3a78c8dff060509c51a234549f43f10e16bddb97
Wxy <wangxiyu191@gmail.com>
2017-10-31 23:47:12 +0800

    connect to AI

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var child_process = require('child_process');
 
 app.use(express.static('static'));
 app.get('/', function(req, res){
@@ -9,9 +10,28 @@ app.get('/', function(req, res){
 });
 io.on('connection', function(socket){
   console.log('a user connected');
+  var aiProcess = child_process.spawn('/Users/wxy/Projects/cpp/Othello_Server/main',[1]
+    , { stdio: ['pipe', 'pipe', process.stderr] });
+  aiProcess.stdout.on('data', function (data) {
+    
+    data = data.toString('ascii').trim();
+    console.log('stdout: ' + data);
+    pos = data.split(',');
+    socket.emit('choose',{
+        "row":pos[0],
+        "col":pos[1]
+    });
+  });
   socket.on('disconnect', function(){
     console.log('user disconnected');
+    aiProcess.kill();
   });
+  socket.on('choose', function(msg){
+    console.log('human choose:' + msg["row"]+","+msg["col"]);
+    aiProcess.stdin.write(msg["row"]+" "+msg["col"] + "\n");
+  });
+
+
 });
 
 http.listen(4000, function(){
